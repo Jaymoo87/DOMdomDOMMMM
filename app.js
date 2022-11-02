3f198829350740f032da38ee69730c6f4bf44dd9
Justin Murrah <jdmurrah@gmail.com>
2022-11-02 13:41:16 -0500

    made a button

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const btn = document.createElement("BUTTON")
+    let btntext = document.createTextNode('Add Square')
+    document.body.appendChild(btn)
+    btn.appendChild(btntext)
+    
+
+    button.addEventListener('click', function (){
+
+
+    })
+
+});
