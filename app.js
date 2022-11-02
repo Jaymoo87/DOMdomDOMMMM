02eab6b3a82f374868f8e21a44de69bd2eacdda0
Justin Murrah <jdmurrah@gmail.com>
2022-11-02 17:54:07 -0500

    parent and child divs, started square logic

--- app.js
+++ app.js
@@ -1,14 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
+   
+    
+    //button
+    const btn = document.createElement("BUTTON");
+    let btntext = document.createTextNode('Add Square');
+    //let butdiv = document.createElement('div')
+    //document.body.appendChild(butdiv)
+    document.body.appendChild(btn);
+    btn.appendChild(btntext);
+    
+    //parent div
+    const daddydiv = document.createElement('div');
+    daddydiv.className = "thedivs";
+    document.body.appendChild(daddydiv);
+    
+    //child divs
+    let div = document.createElement('div')
+    div.className = "squares"
 
-    const btn = document.createElement("BUTTON")
-    let btntext = document.createTextNode('Add Square')
-    document.body.appendChild(btn)
-    btn.appendChild(btntext)
+    daddydiv.appendChild(div)
+    
+    //put button on html document
     
 
-    button.addEventListener('click', function (){
+    
+    
+    let squareCount = 0;
+    
+    
+    
+    // creating squares on click, in-line, and sequenced
+    btn.addEventListener('click', function (){
+        let div = document.createElement('div');
+        let p = document.createElement('p');
+        squareCount ++;
+        daddydiv.appendChild(div)
+        div.appendChild(p);
+        p.className = 'p';
+        p.innerText = squareCount;
+        p.style.color = "black";
+
+        
+        console.log(daddydiv);
+        console.log(p);
 
 
     })
-
+    
+    div.id = squareCount
 });
