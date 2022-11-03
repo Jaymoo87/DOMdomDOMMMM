0a48df6d8488a2e598c60de86cdfae46089def8f
Justin Murrah <jdmurrah@gmail.com>
2022-11-02 22:18:40 -0500

    them blocks are linin' up! divs are sorted out...mostly

--- app.js
+++ app.js
@@ -1,51 +1,43 @@
 document.addEventListener("DOMContentLoaded", function () {
    
     
-    //button
+    //button with a div to seperate from daddydiv
     const btn = document.createElement("BUTTON");
     let btntext = document.createTextNode('Add Square');
-    //let butdiv = document.createElement('div')
-    //document.body.appendChild(butdiv)
+    let butdiv = document.createElement('div')
+    //put button on html document above parent element
     document.body.appendChild(btn);
     btn.appendChild(btntext);
+    document.body.appendChild(butdiv)
     
     //parent div
     const daddydiv = document.createElement('div');
     daddydiv.className = "thedivs";
     document.body.appendChild(daddydiv);
     
-    //child divs
-    let div = document.createElement('div')
-    div.className = "squares"
-
-    daddydiv.appendChild(div)
+    //child divs with class name square for styling
+    let div = document.createElement('div');
     
-    //put button on html document
-    
-
     
     
     let squareCount = 0;
     
-    
-    
     // creating squares on click, in-line, and sequenced
     btn.addEventListener('click', function (){
+        
         let div = document.createElement('div');
         let p = document.createElement('p');
+        div.className = "squares";
         squareCount ++;
-        daddydiv.appendChild(div)
+        daddydiv.appendChild(div);
         div.appendChild(p);
         p.className = 'p';
         p.innerText = squareCount;
         p.style.color = "black";
-
-        
-        console.log(daddydiv);
-        console.log(p);
-
-
-    })
+        console.log(div)
+    });
     
-    div.id = squareCount
+    // putting all the babies with the daddydiv
+    daddydiv.appendChild(div);
+    div.id = squareCount;
 });
